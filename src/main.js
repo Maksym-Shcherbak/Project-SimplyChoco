ed3c1bba25a6426921c39d8185c5d5f843b8219c
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-17 21:13:53 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+import "./js/mobile-menu.js";
 import "./js/reviews-swiper.js";
 import "./js/subscribe-modal.js";
 import "./js/scroll.js";
 import "./js/buy-now-modal.js";
-import "./js/mobile-menu.js";
