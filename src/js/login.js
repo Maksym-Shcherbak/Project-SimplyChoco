28f4fc60b90c0ecc536cac244d3c929a94192eec
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-18 17:51:07 +0300

    style for login form and script for modal

--- src/js/login.js
+++ src/js/login.js
@@ -1,16 +1,44 @@
 (() => {
-	const login = {
-		openModalBtn: document.querySelector(".login-open"),
-		closeModalBtn: document.querySelector(".login-close"),
-		modal: document.querySelector(".login-form"),
-	};
+	const modalBtns = document.querySelectorAll("[data-modal-open]");
+	const closeModalBtns = document.querySelectorAll("[data-modal-close]");
+	const modals = document.querySelectorAll("[data-modal]");
 
-	login.openModalBtn.addEventListener("click", toggleModal);
-	login.closeModalBtn.addEventListener("click", toggleModal);
+	modalBtns.forEach(btn => {
+		btn.addEventListener("click", () => {
+			const modalId = btn.getAttribute("data-modal-open");
+			const modal = document.querySelector(`[data-modal="${modalId}"]`);
+			modal.classList.add("is-open");
+			document.body.classList.add("modal-open");
+		});
+	});
 
-	function toggleModal() {
-		login.modal.classList.toggle("is-hidden");
-	}
+	closeModalBtns.forEach(btn => {
+		btn.addEventListener("click", () => {
+			const modal = btn.closest("[data-modal]");
+			modal.classList.remove("is-open");
+			document.body.classList.remove("modal-open");
+		});
+	});
+
+	document.addEventListener("keydown", event => {
+		if (event.key === "Escape") {
+			modals.forEach(modal => {
+				if (modal.classList.contains("is-open")) {
+					modal.classList.remove("is-open");
+					document.body.classList.remove("modal-open");
+				}
+			});
+		}
+	});
+
+	modals.forEach(modal => {
+		modal.addEventListener("click", event => {
+			if (event.target === modal) {
+				modal.classList.remove("is-open");
+				document.body.classList.remove("modal-open");
+			}
+		});
+	});
 })();
 
 // Show/hide password onClick of button using Javascript only
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./js/mobile-menu.js";
-// import "./js/login.js";
+import "./js/login.js";
 import "./js/reviews-swiper.js";
-import "./js/subscribe-modal.js";
+// import "./js/subscribe-modal.js";
 import "./js/scroll.js";
 import "./js/buy-now-modal.js";
