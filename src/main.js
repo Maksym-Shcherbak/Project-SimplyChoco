233b2ce3f89c265c4d01e189c564d4fe9cc95418
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-19 19:23:26 +0300

    smooth scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./js/how-its-made.js";
 import "./js/mobile-menu.js";
+import "./js/scroll.js";
 // import "./js/login.js";
 import "./js/swiper.js";
 import "./js/subscribe-modal.js";
-import "./js/scroll.js";
 import "./js/buy-now-modal.js";
