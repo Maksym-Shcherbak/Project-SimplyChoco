fd397bce550fb5f07a4580943d599e7068c61e4e
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-19 21:56:16 +0300

    buy now modal

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import "./js/mobile-menu.js";
 import "./js/scroll.js";
+import "./js/how-its-made.js";
 // import "./js/login.js";
 import "./js/swiper.js";
 import "./js/subscribe-modal.js";
-import "./js/scroll.js";
 import "./js/buy-now-validator.js";
 import "./js/buy-now-modal.js";
-import "./js/how-its-made.js";
-
