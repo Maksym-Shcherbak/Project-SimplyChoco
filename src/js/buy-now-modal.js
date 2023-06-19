84fc50947428c15c01ac0fee7a41dfbde63688e4
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-19 21:22:29 +0300

    buy-now-modal

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -1,6 +1,7 @@
 (() => {
 	const refs = {
 		openModalBtn: document.querySelector("[buy-now-modal-open]"),
+		openModalHeroBtn: document.querySelector("[buy-now-hero]"),
 		closeModalBtn: document.querySelector("[buy-now-modal-close]"),
 		closeSubmitBtn: document.querySelector("[buy-now-submit-close]"),
 		modal: document.querySelector("[buy-now-modal]"),
@@ -9,6 +10,7 @@
 	refs.openModalBtn.addEventListener("click", toggleModal);
 	refs.closeModalBtn.addEventListener("click", toggleModal);
 	refs.closeSubmitBtn.addEventListener("click", toggleModal);
+	refs.openModalHeroBtn.addEventListener("click", toggleModal);
 
 	function toggleModal() {
 		refs.modal.classList.toggle("is-hidden");
