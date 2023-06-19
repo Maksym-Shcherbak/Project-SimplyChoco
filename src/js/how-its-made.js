50eca0d8fe45d65137573744afc9d1cd9673568f
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-06-19 21:02:09 +0300

    lazy js

--- src/js/how-its-made.js
+++ src/js/how-its-made.js
@@ -2,9 +2,10 @@
 // https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
 
 // 1. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+tag.loading = "lazy";
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 2. Get elements from the HTML page
@@ -15,23 +16,23 @@ var playerContentImage = document.getElementById("how-its-made-content-image");
 
 // 3. Set event handler for API "Ready" event
 var player;
-window.onYouTubeIframeAPIReady = function() {
-    // create player
-    player = new YT.Player(playerId, {
-        events: {
-            'onReady': onPlayerReady,
-        }
-    });
-}
+window.onYouTubeIframeAPIReady = function () {
+	// create player
+	player = new YT.Player(playerId, {
+		events: {
+			onReady: onPlayerReady,
+		},
+	});
+};
 
 // 4. When player is ready show play button and add event listener to it to start video
 function onPlayerReady(event) {
-    // set click handler to play video
-    playerPlayButton.addEventListener("click", (event) => {
-        playerPlayButton.hidden = true;
-        playerContentImage.hidden = true;
-        playerIframe.hidden = false;
-        player.playVideo();
-    });
-    playerPlayButton.hidden = false;
+	// set click handler to play video
+	playerPlayButton.addEventListener("click", event => {
+		playerPlayButton.hidden = true;
+		playerContentImage.hidden = true;
+		playerIframe.hidden = false;
+		player.playVideo();
+	});
+	playerPlayButton.hidden = false;
 }
