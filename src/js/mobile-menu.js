dc7058cf7a8c3c338b1094ccc93d87bcac603118
Kimmimaru <125698302+Kimmimaru@users.noreply.github.com>
2023-06-20 19:16:53 +0300

    up

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLinks = document.querySelectorAll(".menu-box-link");
 
   const toggleMenu = () => {
     const isMenuOpen =
